2d19f5b0340ee234495015f219c48f154f494cff
Daniil <bodnia666@gmail.com>
2020-04-17 18:34:07 +0300

    Bodnya Danya

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,44 @@
 package domain;
 
+/**
+ * Calculation and nesting of results
+ * 
+ * @author Kot_Shredingera
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
+	/**
+	 * Determining whether a word is a polydrome
+	 * 
+	 * @param s intput word
+	 * @return boolean value (is polydrome - true; none - false)
+	 */
+	public static boolean Calculate(String s) {
+
+		IsDigit(s);
+
+		s = s.toLowerCase();
+		int i, flag = 0;
+		String wordTest = "";
+
+		for (i = s.length() - 1; i >= 0; i--) {
+			if (s.charAt(i) == s.charAt(flag)) {
+				wordTest = wordTest + s.charAt(i);
+				flag++;
+			}
+		}
+
+		return s.equals(wordTest);
+	}
+
+	/**
+	 * Checking for numbers in a string
+	 * 
+	 * @param s input word
+	 */
+	private static void IsDigit(String s) {
+		if (!s.matches("^\\D*$")) { // содержит цифры
+			System.out.println("Ошибка в строке, исключите цифры");
+			System.exit(0);
+		}
+	}
 }
